80a9e127717f7977addc4888210d5cd5c8770c5f
Anuj Kumar <anujsiwachjaat@icloud.com>
2026-07-10 21:59:26 +0530

    [455] [Time Beats: 61%] [Memory Beats: 61.27%] - Leet2Hub

---
+++ 01-Arrays-and-Hashing/455. Assign Cookies/455. Assign Cookies.java
@@ -0,0 +1,15 @@
+1class Solution {
+2    public int findContentChildren(int[] g, int[] s) {
+3        Arrays.sort(g);
+4        Arrays.sort(s);
+5        int child=0;
+6        int index=0;
+7        while(index<s.length && child<g.length){
+8            if(s[index]>=g[child]){
+9                child++;
+10            }
+11            index++;
+12        }
+13        return child;
+14    }
+15}
