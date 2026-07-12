e27e5490bf2f1b6b06f9870028552f17819104c6
Anuj Kumar <anujsiwachjaat@icloud.com>
2026-07-12 23:30:15 +0530

    [29] [Time Beats: N/A] [Memory Beats: N/A] - Leet2Hub

--- 13-Math/29. Divide Two Integers/29. Divide Two Integers.java
+++ 13-Math/29. Divide Two Integers/29. Divide Two Integers.java
@@ -1,11 +1,10 @@
-import java.math.*;
-class Solution {
-    public int divide(int dividend, int divisor) {
-        
-        int quo = dividend / divisor;
-        if(dividend==Integer.MIN_VALUE && divisor==-1){
-            return Integer.MAX_VALUE;
-        }
-        return quo;
-    }
-}
+1class Solution {
+2    public int divide(int dividend, int divisor) {
+3        int quo = dividend / divisor;
+4        if(dividend==Integer.MIN_VALUE && divisor==-1){
+5            return Integer.MAX_VALUE;
+6        }
+7        // Testing 
+8        return quo;
+9    }
+10}
