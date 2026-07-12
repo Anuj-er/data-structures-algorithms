8c87a9bb2c3d76c10e96072d88c753d6c9150e53
Anuj Kumar <anujsiwachjaat@icloud.com>
2026-07-12 23:39:26 +0530

    [190] [Time Beats: 100.00%] [Memory Beats: 83.87%] - Leet2Hub

---
+++ 13-Math/190. Reverse Bits/190. Reverse Bits.java
@@ -0,0 +1,10 @@
+1class Solution {
+2    public int divide(int dividend, int divisor) {
+3        int quo = dividend / divisor;
+4        if(dividend==Integer.MIN_VALUE && divisor==-1){
+5            return Integer.MAX_VALUE;
+6        }
+7        // Testing2
+8        return quo;
+9    }
+10}
